5798da17f24781a859ce38c16b896883126921bd
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-06-21 20:13:34 +0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AddTransaction } from './components/AddTransaction';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { Transaction } from './components/Transaction';
+import { TransactionList } from './components/TransactionList';
+
+import { Paper } from '@material-ui/core';
+
+
+
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+
+    <Paper style={{ marginTop: 100, marginLeft: 250, marginRight: 250, marginBottom: 100, height: '100vh', backgroundColor: '#fafafa' }} elevation={3}>
+      <Header />
+    </Paper>
   );
 }
 
